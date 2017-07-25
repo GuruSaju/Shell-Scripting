b46bff5c2afe2f3214020bfcb6e92d66ca431221
Seed <seed@ubuntu.(none)>
2017-07-25 07:46:40 -0600

    first shell

---
+++ myfirstshell.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#to display something
+echo "Hello World say something:"
+#reads from the terminal
+read something
+# to display what you read from the terminal
+echo $something
+
+ls
+
+
+
